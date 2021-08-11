b2323f46b469f7dae8b371e29850f2c1add87e46
Josephine Palmhede <ejpalmhede@gmail.com>
2021-08-11 15:42:59 -0400

    unfinished

--- 8-8-2021.py
+++ 8-8-2021.py
@@ -1,5 +1,6 @@
 """
 Solution by Josephine Palmhede for August 8th, 2021 leetcode question
+Guided by https://leetcode.libaoj.in/rank-transform-of-a-matrix.html
 
 Given an m x n matrix, return a new matrix answer where answer[row][col] is the rank of matrix[row][col].
 The rank is an integer that represents how large an element is compared to other elements. It is calculated using the following rules:
@@ -16,26 +17,12 @@ def matrixRankTransform(matrix):
     :type matrix: List[List[int]]
     :rtype: List[List[int]]
     """
-    d = dict()
-    # list(d) -> Return a list of all the keys used in the dictionary d
-    # len(d) -> Return the number of items in the dictionary d
-    # d.items() -> Return a new view of the dictionary’s items ((key, value), ..)
-    # d.keys() -> Return a new view of the dictionary’s keys
-    # d.values() -> Return a new view of the dictionary’s values
-    # d[key] -> Return the item of d with key key. Raises a KeyError if key is not in the map.
-    # d[key] = value -> Set d[key] to value
-    # key in d -> Return True if d has key, else False
-    # iter(d) -> Return an iterator over the keys of the dictionary
-
+    print("given matrix: ", matrix)
     rows = len(matrix)
     columns = len(matrix[0])
 
-    print("given matrix: ", matrix)
-    #sort into ascending order the elements in the same row and column
-    #the rank of element i will be the position of element i in the array, given there are no duplicates.
-        
-    #collect like elements in a dictionary
-    #need to account for situation where there is a duplicate number but it is not in the same column or row...
+    #dictionary to map elements to their location in the matrix
+    d = dict()
     for i in range(rows):
         for j in range(columns):
             element = matrix[i][j]
@@ -44,7 +31,53 @@ def matrixRankTransform(matrix):
             else:
                 d[element].append((i,j))
     print("d: ", d)
-        
+
+    #link the rows and columns
+    #I will use this to find connected elements
+    connections = {} #connections[element] = the connections to element
+    for row in range(rows):
+        for col in range(columns):
+            element = matrix[row][col]
+            if element not in connections:
+                connections[element] = {}
+            if row not in connections[element]:
+                connections[element][row] = []
+            #∼col = −col − 1 and ∼(∼col) = col
+            #used to store columns and rows in the same map, without complication from both having an index 0.
+            if ~col not in connections[element]:
+                connections[element][~col] = []
+            #link row and column
+            connections[element][row].append(~col)
+            connections[element][~col].append(row)
+    print("connections: ", connections)
+
+    """
+    In the example of test 1, the following matrix:
+    [1 2]
+    [2 3]
+    is represented in connections as:
+    {1: {0: [-1], -1: [0]}, 2: {0: [-2], -2: [0], 1: [-1], -1: [1]}, 3: {1: [-2], -2: [1]}}
+    where the positive numbers are the rows and negative numbers are the columns.
+
+    This is interpreted as element 1 is in row 0 and column 0 (~0 = -0 - 1 = -1). Thus through element 1, row 0 has a link to columnn 0 and column 0 has a link to row 0.
+    Element 2 is in row 0 and column 1 (~1 = -1 - 1 = -2), and row 1 and column 0 (~0 = -0 - 1 = -1). 
+    So through element 2, row 0 has a link to column 1, column 1 has a link to row 0, row 1 has a link to column 0, and column 0 has a link to row 1.
+    Element 3 is in row 1 and column 1 (~1 = -1 - 1 = -2), etc.
+    """
+
+    #create an element-index map that separates elements to put connected elements together in the same array
+    elementindex = dict()
+    seen = []
+    for i in range(rows):
+        for j in range(columns):
+            if (i, j) not in seen:
+                seen.append((i,j))
+                element = matrix[i][j]
+                cons = connections[element]
+    
+    
+    
+    
     #collect all elements into a list
     allElements = []
     for i in range(rows):
@@ -59,7 +92,19 @@ def matrixRankTransform(matrix):
     elements = []
     [elements.append(x) for x in allElements if x not in elements]
     print("elements: ", elements)
-        
+    
+    """
+    initial answer to all zero
+    initial rowMax and colMax to all zero
+    for connected_part in sorted_connected_parts:
+        rank = -1
+        for point (i, j) in connected_part:
+            rank = (rank, max(rowMax[i], colMax[i]) + 1)
+        for point (i, j) in connected_part:
+            answer[i][j] = rank
+            update rowMax and colMax
+    """
+    #rank transform
     for key in d.keys():
         rank = elements.index(key) + 1
         print("rank: ", rank)
@@ -69,6 +114,7 @@ def matrixRankTransform(matrix):
             print("(r, c): ", r, c)
             matrix[r][c] = rank
     print(matrix)
+    return matrix
         
 matrix1 = [[1,2],[2,3]]
 output1 = [[1,2],[2,3]]
@@ -78,4 +124,24 @@ matrix3 = [[20,-21,14],[-19,4,19],[22,-47,24],[-19,4,19]]
 output3 = [[4,2,3],[1,3,4],[5,1,6],[1,3,4]]
 matrix4 = [[7,3,6],[1,4,5],[9,8,2]]
 output4 = [[5,1,4],[1,2,3],[6,3,1]]
-matrixRankTransform(matrix4)
+
+print("\nTest 1:")
+if output1 == matrixRankTransform(matrix1):
+    print("pass")
+else:
+    print("fail")
+print("\nTest 2:")
+if output2 == matrixRankTransform(matrix2):
+    print("pass")
+else:
+    print("fail")
+print("\nTest 3:")
+if output3 == matrixRankTransform(matrix3):
+    print("pass")
+else:
+    print("fail")
+print("\nTest 4:")
+if output4 == matrixRankTransform(matrix4):
+    print("pass")
+else:
+    print("fail")
